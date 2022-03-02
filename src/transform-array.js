b86d7002bc187efa626895cd4c4a2be2aaab3a84
Slava-Shchipunov <shchipunovsd@gmail.com>
2022-03-02 15:43:38 +0700

    feat: solve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let resultArray = [];
+  let isSimpleArrays = true;
+
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  for (let i =0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next' || arr[i] === '--discard-prev' || arr[i] === '--double-next' || arr[i] === '--double-prev') {
+      isSimpleArrays = false;
+      break;
+    }
+  }
+
+  if (isSimpleArrays) {
+    resultArray = arr;
+  } else {
+    for (let i = 0, j = 0; i < arr.length; i++) {
+      if (arr[i] !== '--discard-next' && arr[i] !== '--discard-prev' && arr[i] !== '--double-next' && arr[i] !== '--double-prev') {
+        resultArray[j] = arr[i];
+        j++;
+      } else if (arr[i] === '--discard-next' && typeof arr[i + 1] !== 'undefined') {
+        i++;
+      } else if (arr[i] === '--discard-prev' && typeof arr[i - 1] !== 'undefined' && arr[i - 2] !== '--discard-next') {
+        resultArray.pop;
+        j--;
+      } else if (arr[i] === '--double-next' && typeof arr[i + 1] !== 'undefined') {
+        resultArray[j] = arr[i + 1];
+        resultArray[j + 1] = arr[i + 1];
+        j += 2;
+        i++;
+      } else if (arr[i] === '--double-prev' && typeof arr[i - 1] !== 'undefined' && arr[i - 2] !== '--discard-next') {
+        resultArray[j] = arr[i - 1];
+        j++;
+      }
+    }
+  }
+
+  return resultArray;
+
 }
 
 module.exports = {
